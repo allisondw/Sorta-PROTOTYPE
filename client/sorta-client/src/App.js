47fe17b1f2bed7b37648526eecaf44780717bfa2
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-02-01 13:57:54 -0800

    Build CreatePage component, move pixel sorting and canvas over from main page

--- client/sorta-client/src/App.js
+++ client/sorta-client/src/App.js
@@ -1,6 +1,7 @@
 import GalleryPage from "./components/GalleryPage/GalleryPage.js";
 import SelectedImage from "./components/SelectedImage/SelectedImage.js";
 import MainPage from "./components/MainPage/MainPage.js";
+import CreatePage from "./components/CreatePage/CreatePage.js";
 import Header from "./components/Header/Header.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
@@ -11,6 +12,7 @@ function App() {
     <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/create" element={<CreatePage />}/>
         <Route path="/gallery" element={<GalleryPage/>} />
         <Route path="/image/:id" element={<SelectedImage />} />
       </Routes>
